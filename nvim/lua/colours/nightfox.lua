2b96c6c68d1197f9608630222ea56205ba33be8a
Ammar Arjomand <persiancookbook@gmail.com>
2023-09-07 19:26:40 +0100

    colours, themes & fonts

--- nvim/lua/colours/nightfox.lua
+++ nvim/lua/colours/nightfox.lua
@@ -1,14 +1,13 @@
 -- Default options
 require('nightfox').setup({
   options = {
-    -- Compiled file's destination location
-    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    compile_path = vim.fn.stdpath("cache") .. "/nightfox", -- Compiled file's destination location
     compile_file_suffix = "_compiled", -- Compiled file suffix
     transparent = false,    -- Disable setting background
     terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false,   -- Non focused panes set to alternative background
+    dim_inactive = true,   -- Non focused panes set to alternative background
     styles = {              -- Style to be applied to different syntax groups
-      comments = "NONE",    -- Value is any valid attr-list value `:help attr-list`
+      comments = "italic",    -- Value is any valid attr-list value `:help attr-list`
       conditionals = "NONE",
       constants = "NONE",
       functions = "NONE",
@@ -29,8 +28,11 @@ require('nightfox').setup({
       dashboard = true,
       diagnostic = true,
       gitsigns = true,
+      illuminate = true,
+      indent_blanklines = true,
       modes = true,
       native_lsp = true,
+      navic = true,
       notify = true,
       nvimtree = true,
       telescope = true,
