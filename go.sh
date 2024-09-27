5540c5321791b1dc35badf574a9995f12c603178
Ammar Arjomand <ammar_arjomand@gallagherre.com>
2024-09-27 14:49:45 +0100

    spit n polish

--- go.sh
+++ go.sh
@@ -68,27 +68,29 @@ function runDotbot() {
   exec ./install
 }
 
-PS3="Select choice: "
+while [ true ]
+  PS3="Select choice: "
 
-select choice in "Authenticate GitHub" "Clone and merge dotfiles" "Run DotBot" "Install Brew" Quit
-do
+  select choice in "Authenticate GitHub" "Clone and merge dotfiles" "Run DotBot" "Install Brew" Quit
+  do
     case $choice in
-        "Authenticate GitHub")
-           authGithub
-	   break;;
-        "Clone and merge dotfiles")
-           cloneAndMerge
-	   break;;
-        "Run DotBot")
-           runDotbot
-	   break;;
-        "Install Brew")
-           installBrew
-	   break;;
-        "Quit")
-           echo "We're done"
-           break;;
-        *)
-           echo "Invalid selection";;
+      "Authenticate GitHub")
+        authGithub 
+        break;;
+      "Clone and merge dotfiles")
+        cloneAndMerge
+        break;;
+      "Run DotBot")
+        runDotbot
+        break;;
+      "Install Brew")
+        installBrew
+        break;;
+      "Quit")
+        echo "We're done"
+        break;;
+      *)
+        echo "Invalid selection";;
     esac
+  done
 done
