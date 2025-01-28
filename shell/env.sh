e8eb89870a53f6152c2f1278f99a95e72a0c2744
Ammar Arjomand <ammar_arjomand@gallagherre.com>
2025-01-28 11:23:09 +0000

    shell true color enabled

--- shell/env.sh
+++ shell/env.sh
@@ -42,6 +42,7 @@ fi
   # export PATH=$PATH:~/Documents/code/ME/bin
 
 # COLOURS
+  export COLORTERM='truecolor'
   export LIGHT_FZF_DEFAULT_OPTS="$FZF_COMMON_OPTS --color=fg:#cb719d,bg:#e6dfd6,hl:#5f87af --color=fg+:#575277,bg+:#e6dfd6,hl+:#5f87af --color=info:#afaf87,prompt:#d7005f,pointer:#d7005f --color=marker:#af5fff,spinner:#5cdeff,header:#87afaf"
   export DARK_FZF_DEFAULT_OPTS="$FZF_COMMON_OPTS --color=fg:#967a87,bg:#203042,hl:#5880a8 --color=fg+:#e8e8e8,bg+:#203042,hl+:#73a6d9 --color=info:#707053,prompt:#994c6d,pointer:#bd7aff --color=marker:#84ff00,spinner:#7f738c,header:#009e9e"
   export DARK_KITTY_THEME='Nightfox'
