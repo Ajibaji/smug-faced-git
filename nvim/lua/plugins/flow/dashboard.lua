74098e74ce142e35c01d081d5b67b94ecd7f013f
Ammar Arjomand <persiancookbook@gmail.com>
2025-07-30 15:50:35 +0100

    nvim dashboard cleanup

--- nvim/lua/plugins/flow/dashboard.lua
+++ nvim/lua/plugins/flow/dashboard.lua
@@ -1,12 +1,11 @@
 return {
   'goolord/alpha-nvim',
-  lazy = false,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local alpha = require 'alpha'
-    local dashboard = require 'alpha.themes.dashboard'
+  event = 'VimEnter',
+  enabled = true,
+  init = false,
+  opts = function()
+    local dashboard = require('alpha.themes.dashboard')
 
-    -- Set header
     dashboard.section.header.val = {
       '',
       '',
@@ -15,33 +14,19 @@ return {
       '',
       '',
       '',
+      '     ⠀⠀⠀⢀⣀⣤⣤⣤⣤⣄⡀⠀⠀⠀⠀',
+      '     ⠀⢀⣤⣾⣿⣾⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀',
+      '     ⢠⣾⣿⢛⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀',
+      '     ⣾⣯⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧',
+      '     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿',
+      '     ⣿⡿⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠻⢿⡵',
+      '     ⢸⡇⠀⠀⠉⠛⠛⣿⣿⠛⠛⠉⠀⠀⣿⡇',
+      '     ⢸⣿⣀⠀⢀⣠⣴⡇⠹⣦⣄⡀⠀⣠⣿⡇',
+      '     ⠈⠻⠿⠿⣟⣿⣿⣦⣤⣼⣿⣿⠿⠿⠟⠀',
+      '     ⠀⠀⠀⠀⠸⡿⣿⣿⢿⡿⢿⠇⠀⠀⠀⠀',
+      '     ⠀⠀⠀⠀⠀⠀⠈⠁⠈⠁⠀⠀⠀⠀⠀⠀',
       '',
-      '                          ›—{zzü6ÞÅÅÅÅÅÅÅÅÅÅÅÅÅgÞ6üzz{—›                          ',
-      '                     {ÇGGgÅggÅgÅgÅgÅgÅggÅggÅgÅgÅÅg—{6GÞíz6ÞÇ{                     ',
-      '               ›íüÅÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅggÇ—    ÏÇÏ— —ÏÇüí›               ',
-      '            ›ÇgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgg Ï6      —Þ6›  ›—ÇÞ—            ',
-      '         {6ÅÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅÅggggggggggÅÅgg  ›Ç{       zÞízÞz ›6ü{         ',
-      '       ÏggÅgÅgÅgÅgÅgÅgÅgÅgÅgÅÅgü—››     íí      ››    6—     —ÇGzG›     —ÞÏ       ',
-      '     zggÅgÅgÅgÅgÅgÅgÅgÅgÅgÇ—            {í             GízüÇÏ     üü      ›6z     ',
-      '   —GgÅgÅgÅgÅgÅgÅgÅgÅgg{íÇ{ÇGgÞí››      íí      ››íÇgGü—Ç›         {g       ›Þ{   ',
-      '  zÅgÅgÅgÅgÅgÅgÅgÅgÅÞ{  Ç{        ›—íÏüüÇÇüüÏz{›        íz          ›Þ—       üz  ',
-      ' ÏgÅgÅgÅgÅgÅgÅgÅgÅgü   {Ç               {í              ›Ç           {ü        zü ',
-      '—gÅgÅgÅgÅgÅgÅgÅgÅgí    G—               íí               6—           6{        ü{',
-      'ÇgÅgÅgÅgÅgÅgÅgÅgÅÏ    ›G                íí               zÏ           —Å        {6',
-      'ggÅgÅgÅgÅgÅgÅgÅggí    ›G                íí               —Þ            g—       ›Ç',
-      'ggÅgÅgÅgÅgÅgÅgÅggz{{{{{G{{{ÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅÅí{íg{{{{{{{{{{{—ÞÏ{í{í{í{íÞ',
-      'ÇgÅgÅgÅgÅgÅgÅgÅggz    ›G  ›ÅÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅ› {Ç            Å›       {6',
-      '—ÅgÅgÅgÅgÅgÅgÅgÅgg›    Þ{ ›ÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅÅ— üí           z6        ü{',
-      ' ügÅgÅgÅgÅgÅgÅgÅgÅg—   —Ç ›ÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅggÅ› Ç›          ›Ç›       í6 ',
-      '  ügÅgÅgÅgÅgÅgÅgÅgÅg{   6— —————ÅÅgÅgÅgÅgÅgÅgÅgÅgG————— íü           Ïz       Ïü  ',
-      '   íggÅgÅgÅgÅgÅgÅgÅgÅgz {Ï ›íüü6ÅgÅgÅgÅgÅgÅgÅgÅgÅg6üÏí› ü{          6Ï      ›6í   ',
-      '     ÇgÅgÅgÅgÅgÅgÅgÅgÅgÅÅÅz›    ÅÅgÅgÅgÅgÅgÅgÅgÅgÞ     üggí›      ›Þ—      üü     ',
-      '      —6ÅgÅgÅgÅgÅgÅgÅgÅggÅggÞüz›ÅÅgÅgÅgÅgÅgÅgÅgÅgÞ    íÇ   —ÏÇü› Ï6›    —6ü›      ',
-      '         ÏgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÞ   {ü        ígÇ›   zÅ{         ',
-      '           —ÏgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅÞ ›ÇÏ      —6ü›  íg6z›           ',
-      '               —ÞgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅÞíÇ›    —GÇ››—ÞGz                ',
-      '                   —züGÅÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅgÅG› {ü66Þ6Ç6Ï{›                   ',
-      '                          ›{zÞggGggggÅÅÅÅÅÅÅggggGggGü{›                           ',
+      '',
       '',
       '',
       '',
@@ -51,19 +36,52 @@ return {
       '',
     }
 
-    -- Set menu
     dashboard.section.buttons.val = {
+      dashboard.button('n', ' New file', [[<cmd> ene <BAR> startinsert <cr>]]),
       dashboard.button('r', ' Restore', [[<cmd> lua require("persistence").load() <cr>]]),
-      -- dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
-      dashboard.button('s', ' Settings', ':e $MYVIMRC | :cd %:p:h | vsplit . | wincmd h | pwd<CR>'),
-      -- dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+      dashboard.button('s', ' Settings', [[e $MYVIMRC | :cd %:p:h | vsplit . | wincmd h | pwd<CR>]]),
+      dashboard.button('q', ' Quit', [[<Cmd>qa<CR>]]),
     }
 
-    alpha.setup(dashboard.opts)
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = 'AlphaButtons'
+      button.opts.hl_shortcut = 'AlphaShortcut'
+    end
 
-    -- Disable folding on alpha buffer
-    --     vim.cmd [[
-    --     autocmd FileType alpha setlocal nofoldenable
-    -- ]]
+    vim.cmd('hi AlphaHeader guifg=#FEBA17')
+
+    dashboard.section.header.opts.hl = 'AlphaHeader'
+    dashboard.section.buttons.opts.hl = 'AlphaButtons'
+
+    dashboard.section.header.opts.position = 'center'
+    dashboard.section.buttons.opts.position = 'center'
+
+    return dashboard
+  end,
+  config = function(_, dashboard)
+    -- close Lazy and re-open when the dashboard is ready
+    if vim.o.filetype == 'lazy' then
+      vim.cmd.close()
+      vim.api.nvim_create_autocmd('User', {
+        once = true,
+        pattern = 'AlphaReady',
+        callback = function()
+          require('lazy').show()
+        end,
+      })
+    end
+
+    require('alpha').setup(dashboard.opts)
+
+    vim.api.nvim_create_autocmd('User', {
+      once = true,
+      pattern = 'LazyVimStarted',
+      callback = function()
+        local stats = require('lazy').stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        dashboard.section.footer.val = '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
   end,
 }
