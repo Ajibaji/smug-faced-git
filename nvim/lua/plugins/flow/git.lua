641f0d94ff4136ab2dbfc3da80bf6667d2c141fa
Ammar Arjomand <persiancookbook@gmail.com>
2025-08-14 10:39:40 +0100

    nvim - lazygit cleanup

--- nvim/lua/plugins/flow/git.lua
+++ nvim/lua/plugins/flow/git.lua
@@ -1,31 +1,13 @@
 return {
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "[L]azy[G]it" }
-    }
+  'kdheepak/lazygit.nvim',
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
   },
-  {
-    'lewis6991/gitsigns.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    config = function()
-      require('gitsigns').setup()
-    end,
+  keys = {
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = '[L]azy[G]it' },
   },
 }
