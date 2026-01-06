fde2bcb16600439e75e853428e1262dbca13b7d3
Ammar Arjomand <persiancookbook@gmail.com>
2026-01-06 12:46:08 +0000

    dotbot

--- dotbot-run.sh
+++ dotbot-run.sh
@@ -3,6 +3,7 @@
 DOTBOT_DIR="dotbot"
 DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+STASH=0
 
 function printHeading() {
   printf "%119s\n" ${@}— | sed -e 's/ /—/g';
@@ -17,11 +18,15 @@ function runSymlink() {
 }
 
 function tearDown() {
-  git stash pop
+  [[ $STASH -gt 0 ]] && git stash pop
 }
+
 trap 'tearDown' SIGHUP SIGINT SIGQUIT SIGTERM SIGABRT
+
 cd "${BASEDIR}"
-git stash push --all
+STASH=$(git status -u -s | wc -l)
+[[ $STASH -gt 0 ]] && git stash push --all
+
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive
 
