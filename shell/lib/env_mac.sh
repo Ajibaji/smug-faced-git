12676998214cc3583beaa8641661a8a4c2393ded
Ammar Arjomand <persiancookbook@gmail.com>
2026-04-30 15:02:40 +0100

    shell: for MacOS, added homebrew path helper to environment

--- shell/lib/env_mac.sh
+++ shell/lib/env_mac.sh
@@ -7,7 +7,13 @@
 # this file is sourced BEFORE .bashrc or .zshrc so brew is not in
   if [[ "$OS" == "MacOS" ]]; then
     export HOMEBREW_BUNDLE_FILE=${HOME}/.config/brew/Brewfile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+    # instead of time-consuming `brew shellenv`:
+    export HOMEBREW_PREFIX="/opt/homebrew";
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+    export HOMEBREW_REPOSITORY="/opt/homebrew";
+    eval "$(/usr/bin/env PATH_HELPER_ROOT="/opt/homebrew" /usr/libexec/path_helper -s)"
+    [ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
+    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
   fi
 
 # CA-Certs
